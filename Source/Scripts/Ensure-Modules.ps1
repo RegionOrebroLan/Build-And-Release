370490120cd2ec3e2f55eb96915e2181f048da64
Hans Kindberg <hans.kindberg@gmail.com>
2019-01-25 07:19:05 +0100

    Rewrite of Ensure-Modules.ps1.

--- Source/Scripts/Ensure-Modules.ps1
+++ Source/Scripts/Ensure-Modules.ps1
@@ -1,32 +1,16 @@
-$_validationCommandKey = "ValidationCommand";
+# Module "RegionOrebroLan.Transforming", https://www.powershellgallery.com/packages/RegionOrebroLan.Transforming/, requires .NET Framework 4.6 or higher.
 
-# Module "RegionOrebroLan.Transforming" must be published to https://www.powershellgallery.com/. At the moment its not possible. Waiting for reply.
-# Module "RegionOrebroLan.Transforming" requires .NET Framework 4.6 or higher.
-$requiredModules = @{
-	"RegionOrebroLan.Transforming"=@{
-		$_validationCommandKey="New-PackageTransform"
-	}
-};
+$requiredModules = @("RegionOrebroLan.Transforming");
 
-$currentErrorActionPreference = $ErrorActionPreference;
-$ErrorActionPreference = "Stop";
-
-foreach($key in $requiredModules.Keys)
+foreach($module in $requiredModules)
 {
-	try
+	if(Get-Module -ListAvailable -Name $module)
 	{
-		$command = Get-Command -Name $requiredModules[$key][$_validationCommandKey];
-
-		# Information
-		Write-Host "DLL: $($command.DLL)";
-		Write-Host "Implementing-type: $($command.ImplementingType)";
-		Write-Host "Module: $($command.Module)";
-	}
-	catch
+		Write-Host "Module ""$($module)"" already installed.";
+	} 
+	else
 	{
-		Write-Host "Installing module ""$($key)""...";
-		Install-Module -Name $key -Scope CurrentUser -Force;
+		Write-Host "Installing module ""$($module)""...";
+		Install-Module -Force -Name $module -Scope CurrentUser;
 	}
 }
-
-$ErrorActionPreference = $currentErrorActionPreference;
